18329550788512f11987251631e1d80e72d8098c
Lukas Knoepfel <shylux@gmail.com>
2017-05-22 17:39:28 +0200

    Added HoverBoots.

---
+++ Assets/HoverBoots.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class HoverBoots : MonoBehaviour {
+
+	private Rigidbody _rigid;
+
+	void Start () {
+		_rigid = GetComponent (typeof(Rigidbody)) as Rigidbody;
+	}
+
+	void Update () {
+		_rigid.useGravity = Physics.Raycast(transform.position + Vector3.up, Vector3.down);
+	}
+}
